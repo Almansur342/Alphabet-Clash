dd55f4cea2a23606e6c72597bf7b12622908abc3
Almansur342 <abdullahalmansur2002@gmail.com>
2024-02-15 22:16:00 +0600

    setting score and life

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,12 +1,42 @@
-function continueGame(){
- const alphabet = getRandomAlphabet();
- const currentAlphabet = document.getElementById('current-alphabet');
+document.addEventListener('keyup', function (event) {
+     const playerPressed = event.key;
+     const currentAlphabetElement = document.getElementById('current-alphabet');
+     const currentAlphabet = currentAlphabetElement.innerText;
+     const expectedElement = currentAlphabet.toLowerCase();
+  if (playerPressed == expectedElement) {
+    const currentScore = getScoreLife('current-score');
+    const newScore = currentScore + 1;
+    setScoreLife('current-score', newScore);
+    removeBackgroundColor(expectedElement);
+    continueGame();
+  } else {
+    const currentLife = getScoreLife('current-life');
+    const newLife = currentLife - 1;
+    setScoreLife('current-life', newLife);
+    if(newLife === 0){
+      gameOver()
+    }
+  }
+})
+
+function continueGame() {
+  const alphabet = getRandomAlphabet();
+  const currentAlphabet = document.getElementById('current-alphabet');
   currentAlphabet.innerText = alphabet;
   setBackgroundColor(alphabet);
+
 }
 
-function play(){
+function play() {
   hideElement('home-screen');
+  hideElement('final-score')
   showElement('play-ground');
+  setScoreLife('current-life', 5);
+  setScoreLife('current-score', 0)
   continueGame();
+}
+
+function gameOver(){
+ hideElement('play-ground');
+ showElement('final-score');
 }
--- js/utility.js
+++ js/utility.js
@@ -11,6 +11,21 @@
    const element = document.getElementById(elementId);
    element.classList.add('bg-orange-400');
  }
+ function removeBackgroundColor(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-orange-400');
+ }
+
+ function getScoreLife(elementId){
+  const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+ }
+ function setScoreLife(elementId, value){
+    const element = document.getElementById(elementId);
+     element.innerText = value;
+ }
 
  function getRandomAlphabet(){
   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
@@ -20,3 +35,5 @@
   const alphabet = alphabets[index];
    return alphabet;
  }
+
+  
